293aca4408b3003c8b6cdf9a90074923473f288f
xhb <1032166023@qq.com>
2014-11-12 16:07:26 +0800

    Initial Commit

---
+++ XHBFoundation/XHBFoundation.h
@@ -0,0 +1,19 @@
+//
+//  XHBFoundation.h
+//  XHBFoundation
+//
+//  Created by weqia on 14-11-12.
+//  Copyright (c) 2014年 xhb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XHBFoundation.
+FOUNDATION_EXPORT double XHBFoundationVersionNumber;
+
+//! Project version string for XHBFoundation.
+FOUNDATION_EXPORT const unsigned char XHBFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XHBFoundation/PublicHeader.h>
+
+
